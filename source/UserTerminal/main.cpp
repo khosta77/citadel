67318c0ef13d63d706b55aeffd0fb5a8f421c7d0
khosta77 <puwerfulpants@gmail.com>
2025-04-27 18:00:52 +0300

    Вырезал терминал для пользователя, не успевал настроить

--- source/UserTerminal/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "UserTerminal.hpp"
-#include "utils.hpp"
-
-int main( int argc, char* argv[] )
-{
-    IpFromMainInput address_( argc, argv );
-    UserTerminal user_( address_.ip_, address_.port_, "User" );
-    return user_.run( std::cin );
-}
